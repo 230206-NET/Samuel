7891492a5a842bf0cee587630b6f75bd4bf2f0dd
Samuel Perrino <samuelperrino@Samuels-MacBook-Pro.local>
2023-02-07 17:00:40 -0500

    Tuesday Demos

---
+++ firstCSharp/Program.cs
@@ -0,0 +1,32 @@
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Starting Program...");
+
+string userInput = Console.ReadLine();
+
+Console.WriteLine(userInput);
+
+
+if(5>4)
+{
+Console.WriteLine("Five is greater than Four");
+}
+
+if(4>5)
+{ 
+Console.WriteLine("Four is not greater than Five"); 
+}
+
+int balance = -10;
+
+if(balance <= 0)
+{ 
+Console.WriteLine("Balance is 0 or negative, please check account");
+}
+else 
+{
+Console.WriteLine(balance); 
+}
+
+
+Console.WriteLine("end of program");
+
