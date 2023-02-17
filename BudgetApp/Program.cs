343aabe43dff5fc0e2fac035a2f27566350f3e4d
Samuel Perrino <samuelperrino@Samuels-MacBook-Pro.local>
2023-02-17 16:20:12 -0500

    Recent files

---
+++ BudgetApp/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Budget App
+Create a console application that will help you track your expenses, and thus survive in this economy!
+Your app should be able to do the following
+- Prompt the user to enter the initial budget
+- Prompt the user to enter expenses with amount and description
+- Display the all expenses and the remaining amount
+*/
+
+Console.WriteLine("Please, enter initial budget");
+
+int budget = int.Parse(Console.ReadLine());
+
+
+
+
---
+++ PaidExpenses/ClassLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace PaidExpenses;
+
+/**
+    -Instance variables: 
+        *int airfare. 
+        *string meals. 
+        *int hotel.
+    -Functionality (MVP):
+       
+        **/
+
+public class Class1
+{
+
+}
